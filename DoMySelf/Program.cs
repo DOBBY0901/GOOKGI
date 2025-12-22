5c3e935a7f6171570cffb705a0b79891ff4aa493
Administrator <ldjshfkd0901@gmail.com>
2025-12-22 13:51:43 +0900

    2025.12.22

--- DoMySelf/Program.cs
+++ DoMySelf/Program.cs
@@ -1,62 +1,182 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DoMySelf
 { 
    
-
     internal class Program
     {
-        
-        static void Main()
+        static void PrintMessage()
         {
-            int[] arrayA = new int[10];
-            int[] arrayB = new int[10];
+            Console.WriteLine("야구 게임에 오신것을 환영합니다.");
+            Console.WriteLine("중복되지 않은 3가지 숫자를 입력해주세요.");
+        }
 
-            int[] arrayResult = new int[arrayA.Length + arrayB.Length];
-
-            for (int i = 0; i < arrayA.Length; i++)
+        static int[] GetRandom()
+        {
+            Random rand = new Random();
+            int[] arrayRand = new int [10];
+            for (int i = 0; i < arrayRand.Length; i++)
             {
-                arrayA[i] = i;
-                arrayB[i] = i + 10;
+                arrayRand[i] = i;
             }
 
-            for (int i = 0; i < arrayA.Length; ++i)
+            for(int i = 0; i < arrayRand.Length; i++)
             {
-                arrayResult[i] = arrayA[i]; 
+                int randResult = rand.Next(0,10);
+                int temp = arrayRand[i];
+                arrayRand[i] = arrayRand[randResult];
+                arrayRand [randResult] = temp;
             }
 
-            for(int i  = 0; i < arrayB.Length; i++)
+            int[] arrayResult = new int [3];
+            
+            for(int i = 0;i < arrayResult.Length; i++)
             {
-                arrayResult[arrayA.Length + i] = arrayB[i];
+                arrayResult[i] = arrayRand[i];
+            }
+
+            return arrayResult;
+           
+        }
+
+        static bool GetChenckNumber(string str)
+        {
+            
+            bool _isNumber = true;
+            bool _isLength = true;
+            bool _isOverlap = true;
+
+            if (str.Length != 3) // 문자열 길이 체크
+            { 
+               _isLength = false;
+                if (str.Length < 3)
+                {
+                    _isLength = false;
+                    Console.WriteLine("문자열의 길이가 3보다 짧습니다.");
+                }
+                else
+                {
+                    _isLength = false;
+                    Console.WriteLine("문자열의 길이가 3보다 깁니다.");
+                }
+            }
+
+            if (_isLength) // 숫자인지 체크
+            {           
+                for (int i = 0; i < str.Length; i++)
+                {
+                    char tempC = str[i];
+
+                    if (tempC < 48 || tempC > 57)
+                    { 
+                        _isNumber = false; 
+                    }
+                }
+            }
+           
+            if (!_isNumber)
+            {
+                Console.WriteLine("문자가 섞여있습니다.");
+            }
+
+            if (_isNumber && _isLength) //중복확인
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    for (int j = i + 1; j < str.Length; j++)
+                    {
+                        if (str[i] == str[j])
+                        {
+                            _isOverlap = false;
+
+                        }
+                    }
+                }  
             }
             
-
-            for (int i = 0; i < arrayResult.Length; i++)
+            if(!_isOverlap)
             {
-               Console.WriteLine(arrayResult[i] + " ");
+                Console.WriteLine("중복되는 숫자가 있습니다.");
             }
 
-            int _length = arrayResult.Length/2;
-            int[] arrayResult_A = new int[_length];
-            int[] arrayResult_B = new int[_length];
-
-            for(int i = 0; i < _length; i++)
+            if(_isLength && _isNumber && _isOverlap)
             {
-                arrayResult_A[i] = arrayResult[i];
-                arrayResult_B[i] = arrayResult[_length + i];
+                return true;
             }
-
-            for (int i = 0; i < arrayResult_A.Length; i++)
-            {
-                Console.WriteLine(arrayResult_A[i] + " ");
-                Console.WriteLine(arrayResult_B[i] + " ");
+            else
+            { 
+                return false; 
             }
-
-            Console.ReadKey();
         }
+
+        static void Main()
+        {
+            PrintMessage();
+            int[] arrayCorrect = GetRandom();
+            int _count = 0;
+
+            while (true)
+            {
+                string inputStr = Console.ReadLine();
+                bool _bool = GetChenckNumber(inputStr);
+              
+                if (_bool)
+                {
+                    _count++;
+                    int _strike = 0;
+                    int _ball = 0;
+              
+                    for(int i =0 ; i < inputStr.Length; i++)
+                    { 
+                        for(int j = 0; j < arrayCorrect.Length; j++)
+                        {
+                            int tempNum = (int)inputStr[i] - 48;
+                            if (tempNum == arrayCorrect[j])
+                            {
+                                if(i==j)
+                                {
+                                    _strike++;
+                                }
+                                else
+                                {
+                                    _ball++;
+                                }
+                            }
+                        }
+                    }
+
+                    Console.WriteLine($"{_strike} 스트라이크");
+                    Console.WriteLine($"{_ball} 볼");
+
+                    //몇번만에 맞췄는가를 알려주고 결과창 출력
+                    //다시 새로운 숫자를 집어서 다시 게임 시작
+
+                    if (_strike == 3)
+                    {
+                        Console.WriteLine($"정답입니다. {_count}번 시도하셨습니다.");
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        PrintMessage();
+                        arrayCorrect = GetRandom();
+                        _count = 0;
+                    }
+                
+                }
+                else
+                {
+
+                }
+
+            }
+              
+        }  
     }
 }
---
+++ TestEX/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+       
+//스탯창
+//화면에 플레이어 이름, hp, att(공격력) 출력 - 완료
+//입력 받을 수 있도록                        -완료
+// 1. 몬스터와 싸운다                        -완료
+// 2. 대기한다                               -완료
+// 3. 이외의 경우는 잘못된입력임을 알려줌.   -완료
+//
+//화면에 창을 띄운다. - 완료
+//입력을 받아서 결과창이 나와야한다. - 완료
+//반복되어야한다. - 완료
+//
+//추가
+//클래스로 만든다. - 완료
+//싸운다 - 이후 플레이어는 상,중,하단 중 선택하여 공격가능
+//몬스터는 세 부위중 랜덤하게 방어
+//방어 성공시 공격무효, 방어 실패시 데미지 들어감
+//몬스터가 죽을 때 까지
+//
+//+ 턴제방식으로 플레이어 공격시 몬스터 공격
+// 플레이어도 상,중,하단의 공격을 막는다
+// 한쪽이 죽을떄까지 반복한다.
+
+public class Program
+{
+   enum Scene
+    {
+        start,
+        fight,
+        pause
+    }
+   
+
+    public class Player
+    {
+        string name = "전사";
+        int hp = 100;
+        int att = 30;
+
+        public void PrintStatus()
+        {
+            Player player = new Player();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"플레이어의 이름   : {player.name}");
+            Console.WriteLine($"플레이어의 체력   : {player.hp}");
+            Console.WriteLine($"플레이어의 공격력 : {player.att}");
+            Console.WriteLine("------------------------------");
+        }
+   
+    }
+
+   
+    static void PrintMessage(Player player)
+    {
+        player. PrintStatus();
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("몬스터와 조우했습니다. 행동을 선택해주세요.");
+        Console.WriteLine("1. 싸운다.");
+        Console.WriteLine("2. 대기한다.");
+        Console.WriteLine("------------------------------");
+    }
+
+    static Scene StartScene(Player player)
+    {
+        
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.D1:
+                    Console.WriteLine("몬스터와 전투를 시작합니다.");
+                    return Scene.fight;
+                case ConsoleKey.D2:
+                    Console.WriteLine("대기합니다.");            
+                    return Scene.pause;
+                default:
+                    Console.WriteLine("잘못된 입력입니다.");
+                    return Scene.start;
+            }
+        
+          
+    }
+
+    static Scene HitPosition()
+    {
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("공격할 위치를 선택해주세요");
+            Console.WriteLine("1. 상단.");
+            Console.WriteLine("2. 중단.");
+            Console.WriteLine("2. 하단.");
+            Console.WriteLine("------------------------------");
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.D1:
+                    Console.WriteLine("상단.");
+                    break;
+                case ConsoleKey.D2:
+                    Console.WriteLine("중단.");
+                    break;
+                default:
+                    Console.WriteLine("하단");
+                    break;
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Player player1 = new Player();
+        Scene scene = new Scene();
+        
+        while (true)
+        {
+            if(scene == Scene.start)
+            {
+                PrintMessage(player1);
+                scene = StartScene(player1);
+            }
+            switch (scene)
+            {
+                case Scene.start:
+                    break;
+                case Scene.fight:
+                    
+                    break;
+                case Scene.pause:
+                    break;
+            }
+        }
+        
+
+        Console.ReadKey();
+        
+    }
+
+}
